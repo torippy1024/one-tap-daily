47509913724c09b3190dcb5ae3cdbc2d59efe450
h.toriyama <toriyamahiroto1128@gmail.com>
2022-10-23 19:45:39 +0900

    ボタン風ラジオボタン

--- src/components/Buttons.tsx
+++ src/components/Buttons.tsx
@@ -1,9 +1,9 @@
 export const ButtonColor = {
-  red: "bg-red-700",
-  green: "bg-green-700",
-  blue: "bg-blue-700",
-  yellow: "bg-yellow-700",
-  gray: "bg-gray-700",
+  red: "bg-red-400 peer-checked:bg-red-900",
+  green: "bg-green-400 peer-checked:bg-green-900",
+  blue: "bg-blue-400 peer-checked:bg-blue-900",
+  yellow: "bg-yellow-400 peer-checked:bg-yellow-900",
+  gray: "bg-gray-400 peer-checked:bg-gray-900",
 };
 
 type ButtonColorType = typeof ButtonColor[keyof typeof ButtonColor];
@@ -15,6 +15,7 @@ export type ButtonType = {
 
 type ButtonsType = {
   buttons: ButtonType[];
+  inputName: string;
 };
 
 export const randomButtonColor = () => {
@@ -22,15 +23,25 @@ export const randomButtonColor = () => {
   return colorList[Math.floor(Math.random() * colorList.length)];
 }
 
-const Buttons = ({buttons}: ButtonsType) => {
+const Buttons = ({buttons, inputName}: ButtonsType) => {
   return (
-    <div>
+    <div className="flex">
       {buttons.map((button, index) => 
-        <div
-          className={`btn ${button.color ?? randomButtonColor()} rounded-full mr-1 mb-1`}
-          key={index}
-        >
-          {button.text}
+        <div key={index}>
+          <input
+            type="radio"
+            id={button.text}
+            value={button.text}
+            name={inputName}
+            className="hidden peer"
+            />
+          <label
+            htmlFor={button.text}
+            // className={`btn rounded-full mr-1 mb-1`}
+            className={`btn ${button.color ?? randomButtonColor()} rounded-full mr-1 mb-1`}
+          >
+            {button.text}
+          </label>
         </div>
       )}
     </div>
--- src/pages/TopPage.tsx
+++ src/pages/TopPage.tsx
@@ -2,6 +2,7 @@ import { Link } from "react-router-dom";
 import Buttons, { ButtonColor, ButtonType } from "../components/Buttons";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import { useForm } from "react-hook-form";
 
 const TopPage = () => {
   const scheduleList = [
@@ -15,14 +16,12 @@ const TopPage = () => {
     return {text: schedule};
   });
 
-  const actionList = [
-    "START",
-    "END",
+  const actionButtons: ButtonType[] = [
+    {text: "START", color: ButtonColor.red},
+    {text: "END", color: ButtonColor.blue},
   ];
 
-  const actionButtons: ButtonType[] = actionList.map((action) => {
-    return {text: action};
-  });
+  const {register, handleSubmit} = useForm();
 
   return  (
       <div data-theme="light" className="flex flex-col min-h-screen">
@@ -32,14 +31,14 @@ const TopPage = () => {
           <div className="text-xl font-bold border-b mb-2">
             予定
           </div>
-          <Buttons buttons={scheduleButtons} />
+          <Buttons buttons={scheduleButtons} inputName="schedule" />
         </div>
 
         <div className="mb-4">
           <div className="text-xl font-bold border-b mb-2">
             アクション
           </div>
-          <Buttons buttons={actionButtons} />
+          <Buttons buttons={actionButtons} inputName="action" />
         </div>
       </div>
       <Footer />
